5478ae3a1ff576e0a3719e06694f17a42fa57455
Thomas Chardo <tchardonnens@juniorisep.com>
2023-02-06 21:17:02 +0100

    using imgix

--- next.config.js
+++ next.config.js
@@ -3,5 +3,24 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
 }
+const isGithubActions = process.env.GITHUB_ACTIONS || false
 
-module.exports = nextConfig
+let assetPrefix = ''
+let basePath = '/'
+
+if (isGithubActions) {
+  const repo = process.env.GITHUB_REPOSITORY.replace(/.*?\//, '')
+
+  assetPrefix = `/${repo}/`
+  basePath = `/${repo}`
+}
+
+module.exports = {
+  assetPrefix: assetPrefix,
+  basePath: basePath,
+  images: {
+    loader: 'imgix',
+    path: 'thomas-chardo',
+  },
+  nextConfig
+}
