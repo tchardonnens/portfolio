382706adec001d0fc22c2ea11ab06554d88fc34e
Thomas Chardo <tchardonnens@juniorisep.com>
2023-03-22 14:13:32 +0100

    fix class to className

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,48 +16,46 @@ export default function Home() {
         <meta property="og:url" content="https://portfolio.thomaschardonnens.com" />
       </Head>
 
-      <body>
-        <div class="flex grow flex-col justify-center items-center min-h-screen">
-          <div class="p-4">
-            <p class="text-lg">
-              Hey! I&apos;m Thomas Chardonnens ✌️
-            </p>
-            <p class="text-sm">
-              CS Student&nbsp;
-              <a href="https://www.isep.fr/">@ISEP&nbsp;</a>
-              and&nbsp;
-              <a href="https://www.berkeley.edu/">@UCBerkeley</a>
-            </p>
-            <br />
-            <ul class="text-sm">
-              <li>
-                <a href='https://twitter.com/ChardonnensT'>
-                  <div class="flex flex-row">
-                    <p>Twitter:&nbsp;</p>
-                    <p class="text-gray-400">@ChardonnensT</p>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a href='https://github.com/tchardonnens'>
-                  <div class="flex flex-row">
-                    <p>GitHub:&nbsp;</p>
-                    <p class="text-gray-400">@tchardonnens</p>
-                  </div>
-                </a>
-              </li>
-              <li>
-                <a href='https://www.linkedin.com/in/thomaschardonnens/'>
-                  <div class="flex flex-row">
-                    <p>LinkedIn:&nbsp;</p>
-                    <p class="text-gray-400">@thomaschardonnens</p>
-                  </div>
-                </a>
-              </li>
-            </ul>
-          </div>
+      <div className="flex grow flex-col justify-center items-center min-h-screen">
+        <div className="p-4">
+          <p className="text-lg">
+            Hey! I&apos;m Thomas Chardonnens ✌️
+          </p>
+          <p className="text-sm">
+            CS Student&nbsp;
+            <a href="https://www.isep.fr/">@ISEP&nbsp;</a>
+            and&nbsp;
+            <a href="https://www.berkeley.edu/">@UCBerkeley</a>
+          </p>
+          <br />
+          <ul className="text-sm">
+            <li>
+              <a href='https://twitter.com/ChardonnensT'>
+                <div className="flex flex-row">
+                  <p>Twitter:&nbsp;</p>
+                  <p className="text-gray-400">@ChardonnensT</p>
+                </div>
+              </a>
+            </li>
+            <li>
+              <a href='https://github.com/tchardonnens'>
+                <div className="flex flex-row">
+                  <p>GitHub:&nbsp;</p>
+                  <p className="text-gray-400">@tchardonnens</p>
+                </div>
+              </a>
+            </li>
+            <li>
+              <a href='https://www.linkedin.com/in/thomaschardonnens/'>
+                <div className="flex flex-row">
+                  <p>LinkedIn:&nbsp;</p>
+                  <p className="text-gray-400">@thomaschardonnens</p>
+                </div>
+              </a>
+            </li>
+          </ul>
         </div>
-      </body>
+      </div>
     </>
   )
 }
