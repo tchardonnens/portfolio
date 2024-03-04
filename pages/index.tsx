f6fc70f3b4981382f1bed813bd383fdfdd2b0fef
Thomas Chardonnens <thomas.chardonnens@qonto.com>
2024-03-04 12:22:39 +0100

    improve current status and adjust links

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,6 @@ import { FaLinkedinIn } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa6";
 import Card from '../components/card'
 import Image from 'next/image';
-import EducationIcon from '../components/educationIcon';
 
 export default function Home() {
   return (
@@ -12,15 +11,22 @@ export default function Home() {
     <>
       <Head>
         <title>Thomas Chardonnens</title>
-        <meta name="description" content="Thomas Chardonnens" />
-        <link rel="icon" href="https://pbs.twimg.com/profile_images/1449466562591723522/3MyO9dFd_400x400.jpg" />
+        <meta name="description" content="Thomas Chardonnens - Personal Website" />
+        <link rel="icon" href="https://pbs.twimg.com/profile_images/1449466562591723522/3MyO9dFd_400x400.jpg" type="image/jpg" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
         <meta property="og:title" content="Thomas Chardonnens" />
-        <meta property="og:description" content="Personal website" />
-        <meta property="og:image" content="https://pbs.twimg.com/profile_images/1449466562591723522/3MyO9dFd_400x400.jpg" />
+        <meta property="og:description" content="Personal Website" />
+        <meta property="og:image" content="https://twitter.com/thomas_chardo/header_photo" />
         <meta property="og:url" content="https://portfolio.thomaschardonnens.com" />
+        <meta property="og:type" content="website" />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Thomas Chardonnens" />
+        <meta name="twitter:description" content="Personal Website" />
+        <meta name="twitter:image" content="https://twitter.com/thomas_chardo/header_photo" />
       </Head>
       <main className="flex flex-col mx-auto justify-center items-center min-h-screen bg-white text-neutral-900 dark:bg-black dark:text-neutral-200 mt-4 mb-4">
         <div className="mt-4 w-5/6 md:w-3/4 lg:w-5/6">
@@ -43,46 +49,51 @@ export default function Home() {
             </a>
           </div>
           <br />
-          <div className="text-md text-center leading-relaxed">
+          <div className="text-lg text-center leading-relaxed">
             <p className='text-2xl'>About me</p>
             <br />
-            CS Master student @&nbsp;
-            <a href="https://www.berkeley.edu/">University of California, Berkeley 🇺🇸</a>
-            &nbsp;and&nbsp;
-            <a href="https://www.isep.fr/">ISEP Paris 🇫🇷</a>
+            <strong>Master&apos;s Candidate in Computer Science</strong>
             <br />
-            ex-Software Engineer Intern @&nbsp;
-            <a href="https://www.ledger.com/">Ledger</a>
-            &nbsp;and&nbsp;
-            <a href="https://www.idemia.com/">Idemia</a>
+            <a href="https://www.berkeley.edu/">University of California, Berkeley 🇺🇸 </a>
+            and
+            <a href="https://www.isep.fr/"> ISEP Paris 🇫🇷</a>
+            <br />
+            Expected Graduation: Summer 2024
             <br />
             <br />
-            Studying about NLP, Machine Learning and AI agents.
+            <strong>Software Engineer Intern</strong>
             <br />
-            My strongest skillset is web development (fullstack) but I am also interested in cybersecurity and blockchain.
+            <a href="https://qonto.com/en">Qonto</a> – European leader in neobanking for SMEs
+            <br />
+            <br />
+            <strong>Previous internships</strong>
+            <br />
+            <a href="https://www.ledger.com/">Ledger</a> – Leader in security for crypto-assets
+            <br />
+            <a href="https://www.idemia.com/">Idemia</a> – Leader in identity technologies
+            <br />
+            <br />
+            <br />
+            Studying NLP, Machine Learning, and AI agents.
+            <br />
+            My strongest skillset is fullstack web development but I am also interested in cybersecurity and blockchain.
             <br />
             As a techno-optimist, I believe in the power of technology to accelerate the growth of the human species.
           </div>
-          <br />
+        <br />
+
 
 
           <h2 className='text-2xl text-center'>Some of my projects</h2>
           <br />
           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
 
-            <Card title="Curious"
-              description="My main side project. Working on a way to change the way we learn by providing custom knowledge just in time by using LLMs."
-              linkProd="https://verycurious.xyz"
-              backendStack="Python, FastAPI, PostgreSQL, Redis"
-              frontendStack="Next.js, TailwindCSS, Vercel">
-            </Card>
-
             <Card title="Tailored tours"
               description="Implementation of the TSP on a database of 2000 real locations in France to help tourists plan their trip according to their time constraints."
               linkProd="https://map.verycurious.xyz"
               linkRepo='https://github.com/tchardonnens/t3-back'
               backendStack='Go, Gin, SQLite'
-              frontendStack='Next.js, TailwindCSS, Vercel'>
+              frontendStack='Next.js, Vercel'>
             </Card>
 
             <Card title="Plane buddy"
@@ -90,7 +101,7 @@ export default function Home() {
               linkProd="https://plane-buddy.vercel.app/"
               linkRepo='https://github.com/tchardonnens/planeBuddy'
               backendStack='Node.js, Next.js, PostreSQL'
-              frontendStack='Next.js, TailwindCSS, Vercel'>
+              frontendStack='Next.js, Vercel'>
             </Card>
 
             <Card title="ScanURL"
@@ -101,19 +112,19 @@ export default function Home() {
               frontendStack='Jinja'>
             </Card>
 
-            <Card title="Password Crusher"
-              description="Website interface to retrieve cached cracked unsalted MD5 passwords"
-              linkRepo='https://github.com/tchardonnens/pwd-crusher-back'
-              backendStack='Nest, Redis'
-              frontendStack='Angular'>
-            </Card>
-
             <Card title="Portfolio"
               description="My personal website. You are on it right now. Using Next because I want to develop something more complex than a static website in the next version..."
               linkProd="https://thomascdnns.com"
               linkRepo="https://github.com/tchardonnens/portfolio"
               backendStack='???'
-              frontendStack='Next.js, TailwindCSS, Vercel'>
+              frontendStack='Next.js, Vercel'>
+            </Card>
+
+            <Card title="Password Crusher"
+              description="Website interface to retrieve cached cracked unsalted MD5 passwords"
+              linkRepo='https://github.com/tchardonnens/pwd-crusher-back'
+              backendStack='Nest, Redis'
+              frontendStack='Angular'>
             </Card>
 
             <Card title="Flat Fall Race"
@@ -150,7 +161,7 @@ export default function Home() {
           <div className="text-md text-center leading-loose">
             <p className='text-2xl'>A little more about me</p>
             <br />
-            21 years old, I love running and Korean food 🇰🇷 (my next trip is definitely Seoul).
+            22 years old, I love running and Korean food 🇰🇷 (my next trip is definitely Seoul).
             <br />
             My favorite books are probably The Hobbit, 1984 and Atomic Habits.
             <br />
